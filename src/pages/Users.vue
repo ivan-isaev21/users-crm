37a543e87ae6b79691cd6ffc94a2801e42d14398
ivan-isaev21 <isaev.ivan.aleksandrovich@gmail.com>
2022-06-21 13:16:46 +0300

    Add sort users

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -2,26 +2,30 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-
+        <p>debug sort:{{currentSort}} direction: {{ currentSortDir}}</p>
         <!-- table -->
         <table>
 
           <!-- head -->
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Gender</th>
+              <th>Image</th>
+              <th @click="sort('firstName')">Name</th>
+              <th @click="sort('age')">Age</th>
+              <th @click="sort('gender')">Gender</th>
             </tr>
           </thead>
 
           <!-- body -->
           <tbody>
-            <tr>
-              <td></td>
+            <tr v-for="user in usersSort" :key="user.id">
+              <td><img :src="user.image" alt="" width="100"></td>
+              <td>{{ user.firstName }}</td>
+              <td>{{ user.age }}</td>
+              <td>{{ user.gender }}</td>
             </tr>
           </tbody>
-        </table>
+        </table>        
 
       </div>
     </section>
@@ -29,13 +33,49 @@
 </template>
 
 <script>
+  import axios from "axios"
   export default {
     data() {
       return {
         users: [
-          { id: 1, name: 'Jack', age: 22, gender: 'male' },
-          { id: 2, name: 'Alex', age: 24, gender: 'male' },
-        ]
+
+        ],
+        currentSort: '',
+        currentSortDir: 'asc'
+
+      }
+    },
+    created() {
+      axios
+        .get('https://dummyjson.com/users/')
+        .then(response => {
+          this.users = response.data.users
+        })
+        .catch(error => console.error(error))
+
+      // this.users = [
+      //   { id: 1, name: 'Jack', age: 22, gender: 'male' },
+      //   { id: 2, name: 'Alex', age: 24, gender: 'male' },
+      // ]
+
+    },
+    computed: {
+      usersSort() {
+        return this.users.sort((a, b) => {
+          let mod = 1
+          if (this.currentSortDir === 'desc') mod = -1
+          if (a[this.currentSort] < b[this.currentSort]) return -1 * mod
+          if (a[this.currentSort] > b[this.currentSort]) return 1 * mod
+          return 0
+        })
+      }
+    },
+    methods: {
+      sort(e) {
+        if (e === this.currentSort) {
+          this.currentSortDir = this.currentSortDir === 'asc' ? 'desc' : 'asc'
+        }
+        this.currentSort = e
       }
     }
   }
